028f76c3d399bc44c35a617af048532f6cc16da6
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-11-04 19:45:26 +0600

    add variant002

---
+++ variant002/Program.cs
@@ -0,0 +1,23 @@
+﻿//
+Console.Write ("Введите значение x");
+int x = int.Parse(Console.ReadLine () ?? "0");
+
+Console.Write ("Введите знаение y");
+int y = int.Parse(Console.ReadLine () ?? "0");
+
+if ((x > 0) && (y > 0))
+{
+   Console.Write ("Значение находиться в I-ой четверти");    
+}
+else if ((x < 0) && (y > 0))
+{
+    Console.Write ("Значение находиться в II-ой четверти");
+}
+else if ((x < 0) && (y < 0))
+{
+    Console.Write ("Значение находиться в III- ой четверти");
+}
+else if ((x > 0) && (y < 0))
+{
+    Console.Write ("Значение находиться в IV-ой четверти");
+}
