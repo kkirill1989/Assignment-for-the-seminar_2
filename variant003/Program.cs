c6c7d3511931b27f54be2fdd87fde1bbc255ab5c
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-11-04 20:12:11 +0600

    add variant003

---
+++ variant003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Проверка на палиндром
+Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine());
+string b = a. ToString();
+if(b[0]==b[4] && b[1] == b[3])
+{
+    Console.WriteLine("Число является палиндромом");
+} 
+else
+{
+    Console.WriteLine("Число не является палиндромом");  
+}
