2f434e139e14c07f439dda6939396d75b27342a2
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-11-04 18:37:00 +0600

    add variant001

---
+++ variant001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 7;
+int c = 23;
+
+if(a% b == 0 && a % c == 0)
+{
+  Console.WriteLine("Заданное число кратно 7 и 23");
+}
+else
+{
+  Console.WriteLine("Заданное число не кратно 7 и 23");   
+}
